85f7edb21c5d974f9028ac50fda86623b6b54391
tsh007 <tejashsingh888@gmail.com>
2023-03-14 15:14:32 +0530

    few_modifications

--- cpp_codes/func7.cpp
+++ cpp_codes/func7.cpp
@@ -1,15 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main(){
+
     int a;
     cin>>a;
-    for(int i=1;i!=0;i=i*10)
-    {
-        
+    
 
-
-
-    }
 
 
     return 0;
